ac75f1651ac2ecdd98ffc61bc603c93dcd241f56
Pedro Sawczuk <pedrosawczuk53@gmail.com>
2026-07-02 16:22:21 -0400

    chore: add env package with zod validation

---
+++ packages/env/src/index.ts
@@ -0,0 +1,8 @@
+import { z } from 'zod'
+
+const envSchema = z.object({
+	NODE_ENV: z.enum(['dev', 'prod', 'test']).default('dev'),
+	PORT: z.coerce.number().default(3000),
+})
+
+export const env = envSchema.parse(process.env)
