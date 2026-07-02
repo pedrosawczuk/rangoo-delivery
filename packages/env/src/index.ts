a8fa22d52d7747c16d9aa4185b1f6cb68bfde1e6
Pedro Sawczuk <pedrosawczuk53@gmail.com>
2026-07-02 17:26:56 -0400

    fix: update api dev script and env parsing

--- packages/env/src/index.ts
+++ packages/env/src/index.ts
@@ -3,6 +3,7 @@ import { z } from 'zod'
 const envSchema = z.object({
 	NODE_ENV: z.enum(['dev', 'prod', 'test']).default('dev'),
 	PORT: z.coerce.number().default(3000),
+	DATABASE_URL: z.string(),
 })
 
 export const env = envSchema.parse(process.env)
