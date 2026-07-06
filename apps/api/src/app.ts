950465c34ce1747334fafd059c75f9c6f0356ea8
Pedro Sawczuk <pedrosawczuk53@gmail.com>
2026-07-06 15:03:19 -0400

    feat: register restaurant routes in api and add dependencies

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -4,10 +4,13 @@ import {
 	serializerCompiler,
 	validatorCompiler,
 } from 'fastify-type-provider-zod'
+import { restaurantRoutes } from './modules/restaurant/restaurant-routes'
 
 export const app = fastify().withTypeProvider<ZodTypeProvider>()
 
 app.setSerializerCompiler(serializerCompiler)
 app.setValidatorCompiler(validatorCompiler)
+
+app.register(restaurantRoutes, { prefix: '/restaurant' })
 
 app.get('/', () => 'Hello World')
