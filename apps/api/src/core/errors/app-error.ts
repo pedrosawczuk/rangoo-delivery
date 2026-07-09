46964ddacfe9045f62d949c121259ecddc9847ff
Pedro Sawczuk <pedrosawczuk53@gmail.com>
2026-07-09 16:37:50 -0400

    feat: add base error handling classes

---
+++ apps/api/src/core/errors/app-error.ts
@@ -0,0 +1,12 @@
+export abstract class AppError extends Error {
+	public readonly statusCode: number
+	public readonly errorCode: string
+
+	constructor(message: string, statusCode: number, errorCode: string) {
+		super(message)
+		this.statusCode = statusCode
+		this.errorCode = errorCode
+
+		Object.setPrototypeOf(this, AppError.prototype)
+	}
+}
