4a168eae790c4b2ce4faae419b77289ee8d7f75d
Pedro Sawczuk <pedrosawczuk53@gmail.com>
2026-07-09 15:04:24 -0400

    feat: add products database schema

---
+++ packages/database/src/schemas/products.ts
@@ -0,0 +1,31 @@
+import {
+	boolean,
+	integer,
+	pgTable,
+	timestamp,
+	uuid,
+	varchar,
+} from 'drizzle-orm/pg-core'
+import { v7 as uuidv7 } from 'uuid'
+import { restaurantTable } from './restaurant'
+
+export const productsTable = pgTable('products', {
+	id: uuid('id')
+		.primaryKey()
+		.$default(() => uuidv7()),
+	name: varchar('name', { length: 100 }).notNull(),
+	description: varchar('description').notNull(),
+
+	isAvailable: boolean('is_available').notNull().default(true),
+	isVegetarian: boolean('is_vegetarian').notNull().default(false),
+
+	restaurantId: uuid('restaurant_id')
+		.notNull()
+		.references(() => restaurantTable.id, { onDelete: 'cascade' }),
+
+	priceInCents: integer('price_in_cents').notNull(),
+	discountPriceInCents: integer('discount_price_in_cents'),
+
+	createdAt: timestamp().defaultNow().notNull(),
+	updatedAt: timestamp().notNull().$onUpdateFn(() => new Date()),
+})
